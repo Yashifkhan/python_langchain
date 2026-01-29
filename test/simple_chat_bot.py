280bce5dd0ef6278a278303494002e525cb912b9
yashif khan <yashifyashifkhan@gmail.com>
2026-01-29 22:53:01 +0530

    learn struture output four types

--- test/simple_chat_bot.py
+++ test/simple_chat_bot.py
@@ -1,17 +1,19 @@
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage,AIMessage,SystemMessage
 load_dotenv()
 
 model =ChatGroq(model="meta-llama/llama-4-scout-17b-16e-instruct")
 
-chat_histroy=[]
+chat_histroy=[
+    SystemMessage("you are a helpful assistant")   
+]
 while True:
     user_input=input("Enter Query : ")
-    chat_histroy.append(user_input)
+    chat_histroy.append(HumanMessage(user_input))
     if(user_input == "exit" or user_input == "buy"):
         break
-    respons=model.invoke(chat_histroy)
-    chat_histroy.append(respons.content)
+    respons=model.invoke(chat_histroy )
+    chat_histroy.append(AIMessage(respons.content))
     print("Ai : ",respons.content)
 print(chat_histroy)
